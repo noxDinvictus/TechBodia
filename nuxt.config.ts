665be2dd39ca2ab4e83954addb57495c6b2aee39
Noxin D Victus <jvcalma24@gmail.com>
2025-02-12 10:35:02 +0700

    fix: update end point, match the env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      API_BASE_URL: process.env.API_BASE_URL,
+      API_BASE_URL: process.env.NUXT_PUBLIC_API_BASE_URL,
     },
   },
 });
