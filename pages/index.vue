6a3e477a2da061b6ef202484b54fdc00f0b74e27
Noxin D Victus <jvcalma24@gmail.com>
2025-02-12 14:41:09 +0700

    fix: image path

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <client-only>
     <div
       class="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat"
-      style="background-image: url('/img/bg.jpg')"
+      style="background-image: url('/TechBodia/img/bg.jpg')"
     >
       <div
         class="max-sm:px-8 rounded-xl bg-gray-600 bg-opacity-40 px-16 py-10 shadow-lg backdrop-blur-md"
