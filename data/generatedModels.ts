90baf75b2d14f4e94bb87f3213e79050f22de568
Noxin D Victus <jvcalma24@gmail.com>
2025-02-14 09:43:42 +0700

    feat: generate latest model

---
+++ data/generatedModels.ts
@@ -0,0 +1,67 @@
+/* Generated Models - DO NOT EDIT */
+import * as dto from './dtos';
+
+export namespace M {
+  export class MetaDTO {
+    currentPage: number;
+    totalPages: number;
+    totalItems: number;
+
+    constructor (data: any) {
+        this.currentPage = data?.currentPage;
+        this.totalPages = data?.totalPages;
+        this.totalItems = data?.totalItems;
+    }
+  }
+
+  export class NoteDTO {
+    noteId: string;
+    title: string;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+    createdByUserId: string;
+
+    constructor (data: any) {
+        this.noteId = data?.noteId ?? '';
+        this.title = data?.title ?? '';
+        this.content = data?.content ?? '';
+        this.createdAt = data?.createdAt ?? '';
+        this.updatedAt = data?.updatedAt ?? '';
+        this.createdByUserId = data?.createdByUserId ?? '';
+    }
+  }
+
+  export class NotePayload {
+    title: string;
+    content: string;
+
+    constructor (data: any) {
+        this.title = data?.title ?? '';
+        this.content = data?.content ?? '';
+    }
+  }
+
+  export class UserDTO {
+    userId: string;
+    username: string;
+    createdAt: string;
+
+    constructor (data: any) {
+        this.userId = data?.userId ?? '';
+        this.username = data?.username ?? '';
+        this.createdAt = data?.createdAt ?? '';
+    }
+  }
+
+  export class UserPayload {
+    username: string;
+    password: string;
+
+    constructor (data: any) {
+        this.username = data?.username ?? '';
+        this.password = data?.password ?? '';
+    }
+  }
+
+}
