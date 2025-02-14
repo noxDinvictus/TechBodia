17efb12f624f34c6b11b83d67d4211d4a606d967
Noxin D Victus <jvcalma24@gmail.com>
2025-02-14 10:21:12 +0700

    feat: create a generate Results interface

--- data/interfaces.ts
+++ data/interfaces.ts
@@ -1,8 +1,15 @@
+import type { M } from './generatedModels';
+
 export namespace I {
   export interface CommonState {
     isCreating: boolean;
     isFetching: boolean;
     isUpdating: boolean;
     isSearching: boolean;
+  }
+
+  export interface Results<T> {
+    items: T[];
+    meta: M.MetaDTO;
   }
 }
