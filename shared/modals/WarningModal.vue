fb1b674e376fc18af81042be75792637b53efb2f
Noxin D Victus <jvcalma24@gmail.com>
2025-02-14 14:24:40 +0700

    feat: create reusable WarningModal component

---
+++ shared/modals/WarningModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <Modal :isOpen="isOpen" title="Warning!" @close="$emit('close')">
+    <p>The action you are about to do is irreversible.</p>
+
+    <p class="my-2 text-center italic">Do you want to continue?</p>
+
+    <template #button>
+      <Button
+        @click="$emit('yes')"
+        class-name="bg-green-300 hover:bg-green-400 text-black"
+        title="Yes"
+        :loading="isLoading"
+      />
+    </template>
+  </Modal>
+</template>
+<script setup lang="ts">
+import Modal from '~/shared/components/Modal.vue';
+import Button from '../components/Button.vue';
+
+interface IProps {
+  isOpen: boolean;
+  isLoading?: boolean;
+}
+
+const props = defineProps<IProps>();
+
+const isOpen = computed(() => props.isOpen);
+const emit = defineEmits(['yes', 'close']);
+</script>
