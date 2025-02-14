7fcc252feb88400b34e2d8fafbe78c86e277a941
Noxin D Victus <jvcalma24@gmail.com>
2025-02-14 14:25:24 +0700

    refactor: rename userName to username

--- shared/forms/UserForm.vue
+++ shared/forms/UserForm.vue
@@ -1,10 +1,10 @@
 <template>
-  <h1 class="mb-2 text-center text-xl text-white">Enter Login Details</h1>
+  <h1 class="mb-2 text-center text-xl text-white">Enter Details</h1>
 
   <form @submit.prevent="handleSubmit">
     <div class="mb-4 text-lg">
       <input
-        v-model="payload.userName"
+        v-model="payload.username"
         class="rounded-3xl border-none bg-white px-6 py-2 text-center shadow-xl outline-none"
         type="text"
         name="username"
@@ -23,7 +23,7 @@
         required
       />
     </div>
-    <div class="mt-8 flex justify-center text-lg text-black">
+    <div class="mt-8 flex justify-center gap-2 text-lg text-black">
       <Button title="Login" type="submit" :loading="isFetching" />
 
       <Button
@@ -31,13 +31,14 @@
         type="button"
         @click="register"
         :loading="isCreating"
+        class-name="bg-green-500 hover:bg-green-600"
       />
     </div>
   </form>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
-import type { User } from '~/data/models/user.model';
+import type { M } from '~/data/generatedModels';
 import { useUserStore } from '~/stores/user/index.module';
 import Button from '../components/Button.vue';
 
@@ -46,19 +47,19 @@ const router = useRouter();
 const userStore = useUserStore();
 const { isCreating, isFetching } = storeToRefs(userStore);
 
-const payload = ref<User.Payload>({
-  userName: '',
+const payload = ref<M.UserPayload>({
+  username: '',
   password: '',
 });
 
 async function register() {
-  if (payload.value.password && payload.value.userName) {
+  if (payload.value.password && payload.value.username) {
     await userStore.create(payload.value);
   }
 }
 
 async function handleSubmit() {
-  if (payload.value.password && payload.value.userName) {
+  if (payload.value.password && payload.value.username) {
     await userStore.authenticate(payload.value);
     router.push('/notes');
   }
